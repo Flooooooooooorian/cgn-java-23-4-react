a2abf75c7c63e49bb63f96690431720858b1d84d
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-01-17 12:56:21 +0100

    Add RickAndMorty

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Hello from "./Hello.tsx";
+import RickAndMorty from "./RickAndMorty.tsx";
 
 function App() {
 
   return (
     <>
       <Hello />
+        <RickAndMorty />
     </>
   )
 }
